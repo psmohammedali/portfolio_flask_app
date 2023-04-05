2baf959ca84047fe410f4a3203c63b9674f5530a
Mohammed Ali P S <48435733+psmohammedali@users.noreply.github.com>
2023-04-05 14:56:54 +0530

    version-1

---
+++ trigger.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,send_from_directory
+my_app_obj = Flask(__name__)
+
+
+@my_app_obj.route("/")
+def hello():
+    return render_template("home.html")
+
+@my_app_obj.route("/index")
+def show():
+    return send_from_directory("static","icons/psmohammedali_CV.pdf")
+
+@my_app_obj.route("/old")
+def old_portfolio():
+    return render_template("old.html")
+
+
+if __name__ == "__main__":
+    my_app_obj.run()
